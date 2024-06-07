7b2dfe447914503157698d58582dbfd3474fd55c
yungetion <shustermanjonah@gmail.com>
2024-06-07 12:56:25 -0700

    2nd try

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, dad!");
         System.out.println("Luke is great!");
+        System.out.println("Lia loves GOT");
     }
 }
